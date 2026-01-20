01ec11611536acb4162ae453748695911b59b91b
Vadym Tymeichuk <40869955+Vadym11@users.noreply.github.com>
2026-01-20 14:51:02 +0000

    regenerate playwright image

--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -1,4 +1,4 @@
-import { test as setup, chromium, expect } from '@playwright/test';
+import { test as setup, expect } from '@playwright/test';
 import path from 'path';
 import { LoginPage } from '../pages/LoginPage';
 import { HomePage } from '../pages/HomePage';
@@ -8,30 +8,25 @@ import { registerRandomUser } from '../utils/test-utils';
 const authFile = path.join(__dirname, '../playwright/.auth/userState.json');
 const userFile = path.join(__dirname, '../playwright/.auth/userData.json');
 
-setup('Authenticate', async ({ request }) => {
-    // 1. Create the user
-    const user = await registerRandomUser(request);
+setup.use({ headless: true });
 
-    // 2. Save User Data (email/password) to its own JSON file
-    // This lets the tests know WHO was registered
-    fs.writeFileSync(userFile, JSON.stringify(user, null, 4));
+setup('Authenticate', async ({ request, page }) => {
+  // 1. Create the user
+  const user = await registerRandomUser(request);
 
-    // 3. Perform Browser Login
-    const browser = await chromium.launch({headless: true});
-    const context = await browser.newContext();
-    const page = await context.newPage();
+  // 2. Save User Data (email/password) to its own JSON file
+  // This lets the tests know WHO was registered
+  fs.writeFileSync(userFile, JSON.stringify(user, null, 4));
 
-    // await page.goto(process.env.BASE_URL!);
-    const homePage = await new HomePage(page).goTo();
-    await homePage.header.clickSignInLink();
-    
-    const myAccountPage = await new LoginPage(page).loginSuccess(user.email, user.password);
+  // 3. Perform Browser Login
+  const homePage = await new HomePage(page).goTo();
+  await homePage.header.clickSignInLink();
 
-    await expect(myAccountPage.myAccountTitle).toHaveText('My account');
+  const myAccountPage = await new LoginPage(page).loginSuccess(user.email, user.password);
 
-    // 4. Save Session State (Cookies/LocalStorage)
-    // This is the file Playwright uses for 'storageState'
-    await page.context().storageState({ path: authFile });
+  // eslint-disable-next-line playwright/no-standalone-expect
+  await expect(myAccountPage.myAccountTitle).toHaveText('My account');
 
-    await browser.close();
+  // 4. Save Session State (Cookies/LocalStorage)
+  await page.context().storageState({ path: authFile });
 });
