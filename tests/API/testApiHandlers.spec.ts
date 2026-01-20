d08c71c69c723e6c856c33fc9ae306bb40bbcee8
Vadym Tymeichuk <40869955+Vadym11@users.noreply.github.com>
2026-01-20 15:53:35 +0000

    fix 1

--- tests/API/testApiHandlers.spec.ts
+++ tests/API/testApiHandlers.spec.ts
@@ -2,6 +2,8 @@ import { test } from '../../fixtures/apiFixtures';
 import { getAPIBaseUrl, getUserDataByEmailAPI } from '../../utils/test-utils';
 import { UserAPI } from '../../types/usersAPI';
 import { PaginatedResponse } from '../../types/api-responses';
+import path from 'path';
+import fs from 'fs';
 
 test('API handlers', async ({ apiHandler }) => {
   const baseAPIUrl = getAPIBaseUrl();
@@ -16,7 +18,9 @@ test('API handlers', async ({ apiHandler }) => {
 });
 
 test('API handler util', async ({ apiHandler }) => {
-  const user = await getUserDataByEmailAPI(apiHandler, 'rosalia.conn@gmail.com');
+  const userPath = path.join(process.cwd(), 'playwright/.auth/userData.json');
+  const userData = JSON.parse(fs.readFileSync(userPath, 'utf-8'));
+  const user = await getUserDataByEmailAPI(apiHandler, userData.email);
 
   console.log(user.id);
   console.log(user.first_name + ' ' + user.last_name);
