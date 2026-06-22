dce5f19b6f37855e7c7c654381596b03fcf9348a
Vadym Tymeichuk <40869955+Vadym11@users.noreply.github.com>
2026-06-22 15:17:16 +0100

    refactor: replace GH_PAT with GITHUB_TOKEN

--- tests/E2E/adminPanel.spec.ts
+++ tests/E2E/adminPanel.spec.ts
@@ -5,15 +5,23 @@ import { AdminProductsPage } from '@pages/admin/AdminProductsPage';
 test.describe('Admin Panel Tests', () => {
   test.use({ storageState: async ({ adminUserStateWorker }, use) => use(adminUserStateWorker) });
 
-  test('TC-AUTH-001 - Admin: Dashboard Loads Correctly', async ({ page }) => {
-    const adminDashboardPage = await new AdminDashboardPage(page).open();
+  test(
+    'TC-AUTH-001 - Admin: Dashboard Loads Correctly',
+    { tag: ['@admin', '@smoke'] },
+    async ({ page }) => {
+      const adminDashboardPage = await new AdminDashboardPage(page).open();
 
-    await adminDashboardPage.assertAdminDashboard();
-  });
+      await adminDashboardPage.assertAdminDashboard();
+    },
+  );
 
-  test('TC-AUTH-002 - Admin: View All Products', async ({ page }) => {
-    const adminProductsPage = await new AdminProductsPage(page).open();
+  test(
+    'TC-AUTH-002 - Admin: View All Products',
+    { tag: ['@admin', '@products'] },
+    async ({ page }) => {
+      const adminProductsPage = await new AdminProductsPage(page).open();
 
-    await adminProductsPage.assertAdminProductsPage();
-  });
+      await adminProductsPage.assertAdminProductsPage();
+    },
+  );
 });
